9db7a2618796715f571647aaa94580ff242efb22
palikisrinu <147064688+palikisrinu@users.noreply.github.com>
2026-03-14 16:47:20 +0530

    Refactor SheetsConfig to improve credential handling

--- src/main/java/com/example/demo/config/SheetsConfig.java
+++ src/main/java/com/example/demo/config/SheetsConfig.java
@@ -1,12 +1,10 @@
 package com.example.demo.config;
 
 import java.io.InputStream;
-import java.util.Collections;
 import java.io.ByteArrayInputStream;
-import org.springframework.core.io.ClassPathResource;
+import java.util.Collections;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
@@ -14,25 +12,25 @@ import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 
 public class SheetsConfig {
-	
-	 public static Sheets getSheetsService() throws Exception {
 
-	       String json = System.getenv("GOOGLE_CREDENTIALS");
+    public static Sheets getSheetsService() throws Exception {
 
-			InputStream stream = new ByteArrayInputStream(json.getBytes());
+        String json = System.getenv("GOOGLE_CREDENTIALS");
 
-	        GoogleCredentials credentials = GoogleCredentials
-	                .fromStream(in)
-	                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+        InputStream stream = new ByteArrayInputStream(json.getBytes());
 
-	        HttpCredentialsAdapter requestInitializer =
-	                new HttpCredentialsAdapter(credentials);
+        GoogleCredentials credentials = GoogleCredentials
+                .fromStream(stream)
+                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
 
-	        return new Sheets.Builder(
-	                GoogleNetHttpTransport.newTrustedTransport(),
-	                GsonFactory.getDefaultInstance(),
-	                requestInitializer)
-	                .setApplicationName("Ledger App")
-	                .build();
-	    }
+        HttpCredentialsAdapter requestInitializer =
+                new HttpCredentialsAdapter(credentials);
+
+        return new Sheets.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Ledger App")
+                .build();
+    }
 }
